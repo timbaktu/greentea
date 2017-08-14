6b69e9a5737c8ddb6269485f35007ebfc9673887
Satya <satyajeet1234@gmail.com>
2017-08-13 22:40:04 -0400

    First backbone use in index page

---
+++ src/main/webapp/js/wtf/views/exercisePlanView.js
@@ -0,0 +1,52 @@
+(function($) {
+
+	var Hello = Backbone.Model.extend({
+		initialize : function() {
+			this.name = 'name'
+		}
+	});
+
+	var HelloView = Backbone.View.extend({
+		tagName : 'li',
+		render : function() {
+			$(this.el).html('Hello ' + this.model.get('name'));
+			return this;
+		}
+	});
+
+	var HelloList = Backbone.Collection.extend({
+		model : Hello,
+		url : 'http://localhost:8080/GreenTea/exerciseplan/'
+	});
+	
+
+	var HelloListView = Backbone.View.extend({
+		
+		el : '#HelloList',
+		
+		initialize : function() {
+			alert('initialize');
+			var self = this;
+			this.collection = new HelloList();
+		    this.collection.fetch().done(function(){
+		        self.render();
+		      });
+		},
+		
+		render:function(){
+			alert('render');
+			debugger;
+			var id = "ep_";
+			var count = 1;
+		    this.collection.each(function(log) {
+		        console.log('log item.', log.attributes.name);
+		        $('#ep_' + count).html(log.attributes.name);
+		        $('#epd_' + count).html(log.attributes.description);
+		        count = count + 1;
+		    });
+		},
+	});
+
+	new HelloListView();
+
+})(jQuery);
