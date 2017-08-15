eb1172ecd02c08cff1780cde8b1e63943df0317b
Satya <satyajeet1234@gmail.com>
2017-08-14 22:09:30 -0400

    Inserting data in exercise plan table

---
+++ src/main/resources/database/sql/1__SQL_insert_data_exerciseplan.sql
@@ -0,0 +1,4 @@
+insert into wf_exerciseplan (plan_name, plan_description, number_of_days, delflag, createddate, updateddate)
+values ('Muscle Mode', 'Follow this exercise plan to build your muscle.', 45, 0, NOW(), NOW());
+
+COMMIT;
