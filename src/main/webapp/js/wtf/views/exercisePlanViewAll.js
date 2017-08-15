9df6461649787958bbdd7629f5f1bf2af8a2823e
Satya <satyajeet1234@gmail.com>
2017-08-15 07:58:12 -0400

    Showing all exercise plan listing on pricing table.

---
+++ src/main/webapp/js/wtf/views/exercisePlanViewAll.js
@@ -0,0 +1,47 @@
+(function($) {
+
+	var Hello = Backbone.Model.extend({
+		initialize : function() {
+			this.name = 'name'
+		}
+	});
+
+	var HelloView = Backbone.View.extend({
+		tagName : 'li',
+		render : function() {
+			$(this.el).html('Hello ' + this.model.get('name'));
+			return this;
+		}
+	});
+
+	var HelloList = Backbone.Collection.extend({
+		model : Hello,
+		url : 'exerciseplan/'
+	});
+	
+
+	var HelloListView = Backbone.View.extend({
+		
+		el : '#exerciselisting',
+		
+		initialize : function() {
+			var self = this;
+			this.collection = new HelloList();
+		    this.collection.fetch().done(function(){
+		        self.render();
+		      });
+		},
+		
+		render:function(){
+			var source = $('#exercise_plan_template').html();
+			var template = Handlebars.compile(source);
+			var html = template(this.collection);
+			
+			$('#exerciselisting').html(html);
+		},
+	});
+
+	new HelloListView();
+
+
+})(jQuery);
