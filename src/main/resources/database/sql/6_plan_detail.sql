7855f25a44a8d2a27c7f9fd407e54709dfe65223
mohammedkashifkhan <mohammedkashifkhan@mohammeshifsair.fios-router.home>
2017-09-17 21:49:49 -0400

    plandetail update

--- src/main/resources/database/sql/6_plan_detail.sql
+++ src/main/resources/database/sql/6_plan_detail.sql
@@ -13,3 +13,29 @@ CREATE TABLE `wethinkfitness`.`wf_plan_detail` (
 
 INSERT INTO `wethinkfitness`.`wf_plan_detail` (`plan_id`, `type_id`, `number_of_exercises`, `Reps`, `frequency`, `delflag`, `createddate`, `updateddate`) VALUES ('1', '1', '8', '200', 'Once', '0', '2017-09-17 09:00:00', '2017-09-17 09:00:00');
 INSERT INTO `wethinkfitness`.`wf_plan_detail` (`plan_id`, `type_id`, `number_of_exercises`, `Reps`, `frequency`, `delflag`, `createddate`, `updateddate`) VALUES ('1', '2', '12', '220', 'Twice', '0', '2017-09-17 09:00:00', '2017-09-17 09:00:00');
+
+ALTER TABLE `wethinkfitness`.`wf_plan_detail` 
+CHANGE COLUMN `plan_id` `plan_id` BIGINT(20) NOT NULL ;
+
+ALTER TABLE `wethinkfitness`.`wf_plan_detail` 
+CHANGE COLUMN `type_id` `type_id` BIGINT(20) NOT NULL ;
+
+
+ALTER TABLE `wethinkfitness`.`wf_plan_detail` 
+ADD INDEX `id_idx` (`plan_id` ASC);
+ALTER TABLE `wethinkfitness`.`wf_plan_detail` 
+ADD CONSTRAINT `id`
+  FOREIGN KEY (`plan_id`)
+  REFERENCES `wethinkfitness`.`wf_exerciseplan` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+  
+  
+  ALTER TABLE `wethinkfitness`.`wf_plan_detail` 
+ADD INDEX `id_idx1` (`type_id` ASC);
+ALTER TABLE `wethinkfitness`.`wf_plan_detail` 
+ADD CONSTRAINT `id`
+  FOREIGN KEY (`type_id`)
+  REFERENCES `wethinkfitness`.`wf_exercise_type` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
