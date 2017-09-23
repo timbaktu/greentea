10ef92629da9985542b251e87a7b0ebd0e4bae0e
Satya <satyajeet1234@gmail.com>
2017-09-23 19:08:10 -0400

    Plan detail controller change

---
+++ src/main/java/com/springmvc/controller/PlanDetailController.java
@@ -0,0 +1,36 @@
+package com.springmvc.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.springmvc.model.PlanDetail;
+import com.springmvc.service.PlanDetailService;
+
+@RestController
+@EnableWebMvc
+public class PlanDetailController {
+
+        @Autowired
+        PlanDetailService service;
+
+    //-------------------Retrieve All Users--------------------------------------------------------
+    @RequestMapping(value = "/plandetail/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+      public ResponseEntity<List<PlanDetail>>  listPlanDetail(@PathVariable("id") int id) {
+        List<PlanDetail> plandetail = service.findPlanDetail(id);
+        if(plandetail.isEmpty()){
+            return new ResponseEntity<List<PlanDetail>>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
+        }
+        return new ResponseEntity<List<PlanDetail>>(plandetail, HttpStatus.OK);
+    }
+
+
+}
