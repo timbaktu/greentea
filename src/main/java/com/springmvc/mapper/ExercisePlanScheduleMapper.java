514fbc30890c7066e8a87477ed6d41e2ede247bf
mohammedkashifkhan <mohammedkashifkhan@mohammeshifsair.fios-router.home>
2017-09-23 22:54:34 -0400

    mapper update

--- src/main/java/com/springmvc/mapper/ExercisePlanScheduleMapper.java
+++ src/main/java/com/springmvc/mapper/ExercisePlanScheduleMapper.java
@@ -1,5 +1,9 @@
 package com.springmvc.mapper;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import com.springmvc.bean.ExercisePlanScheduleBean;
 import com.springmvc.model.ExercisePlanSchedule;
 
@@ -8,12 +12,17 @@ public class ExercisePlanScheduleMapper {
 	public static void createExercisePlanScheduleBean(ExercisePlanScheduleBean bean, ExercisePlanSchedule schedule) {
 		bean.setId(schedule.getId());
 		bean.setTitle(schedule.getName());
+		bean.setDay(schedule.getday());
 		bean.setDescription(schedule.getDescription());
 		bean.setPlanid(schedule.getplanid());
-		bean.setUrl("http://localhost:8080/GreenTea/exercise-plan-details.html?id=" + 1); // schedule.getExerciseTypeId
-		bean.setStartDate("2017-09-07");
+	    SimpleDateFormat formattedDate = new SimpleDateFormat("yyyy-MM-dd");            
+	    Calendar c = Calendar.getInstance();        
+	    c.add(Calendar.DATE, schedule.getday());  // number of days to add      
+	    String plandate = (String)(formattedDate.format(c.getTime()));
+		bean.setUrl("http://localhost:8080/GreenTea/exercise-plan-details.html?id=" + schedule.getExercise_type_id());
+		bean.setStartDate(plandate);
 		//bean.setEndDate("2017-09-10");
-		bean.setClassName("info");
+		bean.setClassName(schedule.getclassName());
 	}
 	
 	public static void createExercisePlanSchedule() {
